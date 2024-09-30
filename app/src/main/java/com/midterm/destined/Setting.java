0f91b953acc3ec05cd6c76868c3f523b27b5f55e
NgKien94 <quangkienofqn@gmail.com>
2024-09-30 19:38:55 +0700

    UI Setting activity favouriteActivity favouriteRow

--- app/src/main/java/com/midterm/destined/Setting.java
+++ app/src/main/java/com/midterm/destined/Setting.java
@@ -1,8 +1,6 @@
 package com.midterm.destined;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Setting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(() -> {
-            // Chuyển sang SettingActivity
-            Intent intent = new Intent(MainActivity.this, Setting.class);
-            startActivity(intent);
-            finish(); // Để kết thúc MainActivity
-        }, 3000); // 3000 milliseconds = 3 giây
-
     }
 }
