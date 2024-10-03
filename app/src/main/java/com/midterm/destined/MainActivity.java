5ea8a35d99b75a538784a8c5f092e001a8a41f73
quien <lyquyen520@gmail.com>
2024-10-03 10:33:09 +0700

    them giao dien search va add story

--- app/src/main/java/com/midterm/destined/MainActivity.java
+++ app/src/main/java/com/midterm/destined/MainActivity.java
@@ -1,6 +1,7 @@
 package com.midterm.destined;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -83,23 +84,23 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 }
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-
-    View mainView = findViewById(R.id.main);
-    ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
-        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        return insets;
-    });
-
-    mainView.setOnClickListener(v -> {
-        Intent intent = new Intent(MainActivity.this, MainUIActivity.class);
-        startActivity(intent);
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    });
-}
+//@Override
+//protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    EdgeToEdge.enable(this);
+//    setContentView(R.layout.activity_main);
+//
+//    View mainView = findViewById(R.id.main);
+//    ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+//        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//        return insets;
+//    });
+//
+//    mainView.setOnClickListener(v -> {
+//        Intent intent = new Intent(MainActivity.this, MainUIActivity.class);
+//        startActivity(intent);
+//        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+//    });
+//}
 
